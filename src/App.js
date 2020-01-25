4ce4c23f201e2032ebbd52dd155d3fa57e3b88a3
GMC2020 <ahmedbalti06@gmail.com>
2020-01-25 17:04:33 +0100

    1st

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import photo from './photo1.jpg';
+import Component from'./Component';
+import './style.css';
+
+function App() {
+
+  let title = "my title";
+
+  return (
+
+    <div className="centent">
+     
+      <h2 className="title">Notre equipe</h2>
+<Component/>
+<Component/>
+<Component/>
+<Component/>
+
+
+  
+ 
+
+    </div>
+
+
+  );
+
+}
+
+export default App;
---
+++ src/Component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import photo from './photo1.jpg';
+import './style.css';
+
+function Component() {
+
+  let title = "my title";
+
+  return (
+
+      <div className="component">
+     
+          <img className="image" src="/photo1.jpg" />
+          <p className="desc">nom <strong >prenom</strong></p>
+          <p className="desc">desc</p>
+          
+         
+          </div>
+
+
+
+
+  );
+
+}
+
+export default Component;
